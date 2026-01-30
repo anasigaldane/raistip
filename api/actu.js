404b8ddb3c1d5347a01303a04a68340a930288c6
Anas Igaldane <Drivetagram@outlook.fr>
2026-01-30 23:13:57 +0100

    add actu api and dependencies

--- api/actu.js
+++ api/actu.js
@@ -1,14 +1,13 @@
+// api/actu.js
 import fs from "fs";
 import path from "path";
 
 export default function handler(req, res) {
   try {
     const filePath = path.join(process.cwd(), "data", "actu.json");
-    const rawData = fs.readFileSync(filePath, "utf-8");
-    const data = JSON.parse(rawData);
-
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
+    const data = fs.readFileSync(filePath, "utf-8");
+    res.status(200).json(JSON.parse(data));
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
   }
 }
