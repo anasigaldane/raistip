6b3a184627d33c0cec8e90056cf744dd06f768be
Anas Igaldane <Drivetagram@outlook.fr>
2026-02-17 19:04:35 +0100

    add actu api and dependencies

--- api/data.js
+++ api/data.js
@@ -10,70 +10,108 @@ export default function handler(req, res) {
         date: "2025-11-14",
         titre: "Avis aux candidats des listes d'attente : Tableau des places vacantes, par ISPITS et par options, session septembre 2025",
         description: "Tableau des places vacantes pour les candidats des listes d'attente, classé par ISPITS et par options.",
-        lien: "https://ispits.sante.gov.ma/Documents/Concoursdacces-licence-septembre2025/orale/RF/Avis%20LA%20et%20places%20vacantes.PDF"
+        lien: "https://ispits.sante.gov.ma/Documents/Concoursdacces-licence-septembre2025/orale/RF/Avis%20LA%20et%20places%20vacantes.PDF",
+        tabl: false
       },
       {
         id: "156",
         date: "2025-10-23",
         titre: "Proclamation des résultats du test d'aptitude du concours d'accès aux ISPITS, cycle de la licence, session septembre 2025",
         description: "Résultats du test d'aptitude pour le concours d'accès aux ISPITS, cycle licence, session septembre 2025.",
-        lien: "https://ispits.sante.gov.ma/Pages/DetailActualites.aspx?IDActu=159"
+        lien: "https://ispits.sante.gov.ma/Pages/DetailActualites.aspx?IDActu=159",
+        tabl: true,
+        tableData: [
+          { "name": "Agadir", "link": "/docs/agadir.pdf" },
+          { "name": "Fès", "link": "/docs/fes.pdf" }
+        ]
       },
       {
         id: "155",
         date: "2025-10-06",
         titre: "Planning du déroulement du test d'aptitude des candidats admis à l'épreuve écrite du concours d'accès aux ISPITS, cycle licence, session septembre 2025",
         description: "Calendrier détaillé du déroulement du test d'aptitude pour les candidats admis.",
-        lien: "https://ispits.sante.gov.ma/Pages/DetailActualites.aspx?IDActu=158"
+        lien: "https://ispits.sante.gov.ma/Pages/DetailActualites.aspx?IDActu=158",
+        tabl: true,
+        tableData: [
+          { "name": "Agadir", "link": "/docs/agadir.pdf" },
+          { "name": "Fès", "link": "/docs/fes.pdf" }
+        ]
       },
       {
         id: "154",
         date: "2025-10-04",
         titre: "Proclamation des résultats de l'épreuve écrite du concours d'accès aux ISPITS, cycle de la licence, session 2025",
         description: "Résultats de l'épreuve écrite pour les candidats du cycle licence, session 2025.",
-        lien: "https://ispits.sante.gov.ma/Pages/DetailActualites.aspx?IDActu=157"
+        lien: "https://ispits.sante.gov.ma/Pages/DetailActualites.aspx?IDActu=157",
+        tabl: true,
+        tableData: [
+          { "name": "Agadir", "link": "/docs/agadir.pdf" },
+          { "name": "Fès", "link": "/docs/fes.pdf" }
+        ]
       },
       {
         id: "153",
         date: "2025-09-18",
         titre: "Listes définitives des candidats présélectionnés par ville d'examen pour passer l'épreuve écrite du concours d'accès aux ISPITS, cycle de la licence, session septembre 2025",
         description: "Les listes définitives des candidats présélectionnés par ville d'examen.",
-        lien: "https://ispits.sante.gov.ma/Pages/DetailActualites.aspx?IDActu=156"
+        lien: "https://ispits.sante.gov.ma/Pages/DetailActualites.aspx?IDActu=156",
+        tabl: true,
+        tableData: [
+          { "name": "Agadir", "link": "/docs/agadir.pdf" },
+          { "name": "Fès", "link": "/docs/fes.pdf" }
+        ]
       },
       {
         id: "152",
         date: "2025-09-02",
         titre: "Listes des candidats présélectionnés par ville d'examen pour passer l'épreuve écrite du concours d'accès aux ISPITS, cycle de la licence, session septembre 2025",
         description: "Listes initiales des candidats présélectionnés par ville d'examen.",
-        lien: "https://ispits.sante.gov.ma/Pages/DetailActualites.aspx?IDActu=155"
+        lien: "https://ispits.sante.gov.ma/Pages/DetailActualites.aspx?IDActu=155",
+        tabl: true,
+        tableData: [
+          { "name": "Agadir", "link": "/docs/agadir.pdf" },
+          { "name": "Fès", "link": "/docs/fes.pdf" }
+        ]
       },
       {
         id: "151",
         date: "2025-07-22",
         titre: "مباراة ولوج المعاهد العليا للمهن التمريضية وتقنيات الصحة 2025/2026",
         description: "AVIS officiel concernant le concours d'accès aux ISPITS pour l'année 2025/2026.",
-        lien: "https://ispits.sante.gov.ma/Pages/DetailActualites.aspx?IDActu=154"
+        lien: "https://drh.sante.gov.ma/Documents/2025/%D8%A5%D8%B9%D9%84%D8%A7%D9%86%20%D9%85%D8%A8%D8%A7%D8%B1%D8%A7%D8%A9%20%D9%88%D9%84%D9%88%D8%AC%20%D8%A7%D9%84%D9%85%D8%B9%D8%A7%D9%87%D8%AF%20%D8%A7%D9%84%D8%B9%D9%84%D9%8A%D8%A7%20%D9%84%D9%84%D9%85%D9%87%D9%86%20%D8%A7%D9%84%D8%AA%D9%85%D8%B1%D9%8A%D8%B6%D9%8A%D8%A9%20%D9%88_250722_165842.pdf",
+        tabl: false
       },
       {
         id: "150",
         date: "2025-02-06",
         titre: "Avis aux candidats des listes d'attente du concours d'accès aux ISPITS, cycle du master, session de novembre 2024",
         description: "Informations pour les candidats en liste d'attente du cycle master, session novembre 2024.",
-        lien: "https://ispits.sante.gov.ma/Pages/DetailActualites.aspx?IDActu=153"
+        lien: "https://ispits.sante.gov.ma/Documents/MASTER2024/avis%20liste%20atten%20master.pdf",
+        tabl: false
       },
       {
         id: "149",
         date: "2025-01-17",
         titre: "Résultats définitifs du concours d'accès aux ISPITS, cycle du master, session du 17 novembre 2024",
         description: "Résultats finaux du concours pour le cycle master, session du 17 novembre 2024.",
-        lien: "https://ispits.sante.gov.ma/Pages/DetailActualites.aspx?IDActu=152"
+        lien: "https://ispits.sante.gov.ma/Pages/DetailActualites.aspx?IDActu=152",
+        tabl: true,
+        tableData: [
+          { "name": "Agadir", "link": "/docs/agadir.pdf" },
+          { "name": "Fès", "link": "/docs/fes.pdf" }
+        ]
       },
       {
         id: "148",
         date: "2024-12-19",
         titre: "Répartition des candidats convoqués pour passer l'épreuve orale du concours d'accès aux ISPITS, cycle du master, session de 17 novembre 2024",
         description: "Détails sur la répartition des candidats pour l'épreuve orale du concours master.",
-        lien: "https://ispits.sante.gov.ma/Pages/DetailActualites.aspx?IDActu=151"
+        lien: "https://ispits.sante.gov.ma/Documents/MASTER2024/R%C3%A9partition%20des%20candidats%20convoqu%C3%A9s%20Oral.pdf",
+        tabl: true,
+        tableData: [
+          { "name": "Agadir", "link": "/docs/agadir.pdf" },
+          { "name": "Fès", "link": "/docs/fes.pdf" }
+        ]
       }
     ]
   };
